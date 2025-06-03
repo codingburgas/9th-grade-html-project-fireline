d7e018f4d291016a7600162a2a24d8219143b7a3
AODorosh <aodorosh23@codingburgas.bg>
2025-06-03 22:54:38 +0300

    Create Main menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,20 @@
-function darkmode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+let darkmode = localStorage.getItem('darkmode')
+const themeswitch = document.getElementById('theme-switch')
 
-
-  function showForm(formId){
-    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
+const enableDarkMode = () =>
+{
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
 }
+const disableDarkMode = () =>
+{
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === 'active') enableDarkMode()
+
+themeswitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+})
