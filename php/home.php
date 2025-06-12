3c1c9dc0bffca66d8163da6b70f3e7dc156294cf
RFHristova23 <rfhristova23@codingburgas.bg>
2025-06-12 04:29:01 +0300

    Add personnel (Close #7)

--- php/home.php
+++ php/home.php
@@ -28,7 +28,7 @@ function isActiveForm($formName, $activeForm){
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Fireline Main Page</title>
   <link rel="stylesheet" href="../css/style.css"/>
-  <link rel="stylesheet" href="../css/statistics.css">
+  <link rel="stylesheet" href="../css/statistics.css"/>
   <script defer src="../javascript/script.js"></script>
 </head>
 <body>
